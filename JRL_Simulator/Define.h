0969df440329c6fe127b49f6e8824d8ebcb27d8a
WAVE <fenix24@naver.com>
2022-01-30 15:23:30 +0900

    [Add] Multicast Class (Header File)

--- JRL_Simulator/Define.h
+++ JRL_Simulator/Define.h
@@ -6,6 +6,9 @@
 #include <System.Classes.hpp>
 
 
+// Define Max Data Size
+#define MCAST_SEND_BUF_SIZE		1500
+#define MCAST_RECV_BUF_SIZE		1500
 
 
 // Define Message Code
--- JRL_Simulator/MulticastSocketThread.h
+++ JRL_Simulator/MulticastSocketThread.h
@@ -3,4 +3,43 @@
 #ifndef MulticastSocketThreadH
 #define MulticastSocketThreadH
 //---------------------------------------------------------------------------
+#include "Define.h"
+#include <System.Classes.hpp>
+
+enum ThreadWorkingType
+{
+	THREAD_STOP = 0,
+	THREAD_RUNNING = 1,
+	THREAD_TERMINATED = 2,
+};
+//---------------------------------------------------------------------------
+
+class CUdpSocketThread : public TThread {
+private:
+	ThreadWorkingType	m_eThreadWork;
+	SOCKET	*m_sock;
+
+protected:
+public: // Variables
+	BYTE sendBuff[MCAST_SEND_BUF_SIZE];
+	BYTE* p_sendText;
+	BYTE recvBuff[MCAST_RECV_BUF_SIZE];
+	//SERVERDATA recvData;
+	bool isTryingToConnect;
+	bool isConnected;
+
+public: // Functions
+	__fastcall CUdpSocketThread(SOCKET *p_sock);
+	__fastcall ~CUdpSocketThread();
+	bool __fastcall Receive();
+	void __fastcall Execute();
+	void __fastcall Stop();
+	void __fastcall Resume();
+	void __fastcall DoTerminate();
+	ThreadWorkingType __fastcall GetThreadStatus();
+};
+
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
 #endif
